9c0de0ad91788e9d7114af43de7c904c2c79041f
PrinceSdeTestcafe <prince.kumar@testerscafe>
2023-01-20 13:15:14 +0530

    FOURTH

--- database/migrations/About/2023_01_19_063646_create_table_about1.php
+++ database/migrations/About/2023_01_19_063646_create_table_about1.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('table_about1', function (Blueprint $table) {
             $table->id();
-            $table->string("image");
+              $table->string("image");
             $table->string("title");
             $table->text("content");
             $table->bigInteger("category_id");
